39aefad76990cacdf2a9509a81fae3a6588fe8f7
R3defined <hallmaurice31@me.com>
2025-06-12 21:51:53 -0400

    Fix background animation: Update Background component script import for production deployment

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_ZEnIuy from "../assets/fractal.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fadding-visuals-exposes-new-dimensions.mdx";
-import __ASTRO_IMAGE_IMPORT_ly10P from "../../assets/pfp.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fmy-first-post.mdx";
 import __ASTRO_IMAGE_IMPORT_1y8Vzg from "../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fspectre.mdx";
-export default new Map([["../assets/fractal.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fadding-visuals-exposes-new-dimensions.mdx", __ASTRO_IMAGE_IMPORT_ZEnIuy], ["../../assets/pfp.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fmy-first-post.mdx", __ASTRO_IMAGE_IMPORT_ly10P], ["../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fspectre.mdx", __ASTRO_IMAGE_IMPORT_1y8Vzg]]);
+import __ASTRO_IMAGE_IMPORT_ly10P from "../../assets/pfp.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fmy-first-post.mdx";
+export default new Map([["../assets/fractal.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fadding-visuals-exposes-new-dimensions.mdx", __ASTRO_IMAGE_IMPORT_ZEnIuy], ["../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fspectre.mdx", __ASTRO_IMAGE_IMPORT_1y8Vzg], ["../../assets/pfp.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fmy-first-post.mdx", __ASTRO_IMAGE_IMPORT_ly10P]]);
 		

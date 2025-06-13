2fa88fd76e885bb6acaf80e0cc86676274c5a611
R3defined <hallmaurice31@me.com>
2025-06-12 21:35:52 -0400

    Fix static deployment: Prerender API routes for static hosting

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/other/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fother%2Fabout.mdx&astroContentModuleFlag=true")],
-["src/content/posts/adding-visuals-exposes-new-dimensions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fadding-visuals-exposes-new-dimensions.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/my-first-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-first-post.mdx&astroContentModuleFlag=true")],
-["src/content/projects/spectre.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fspectre.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/adding-visuals-exposes-new-dimensions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fadding-visuals-exposes-new-dimensions.mdx&astroContentModuleFlag=true")],
+["src/content/projects/spectre.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fspectre.mdx&astroContentModuleFlag=true")],
+["src/content/other/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fother%2Fabout.mdx&astroContentModuleFlag=true")]]);
 		
